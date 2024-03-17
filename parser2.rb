18867c5ece9054083e35c999ee5eafe684957402
Kirill Moiseev <hi.sender@gmail.com>
2024-03-17 20:52:59 +0100

    sleep 0

--- parser2.rb
+++ parser2.rb
@@ -7,9 +7,11 @@ require './proxy_manager'
 
 # book_slug = "21-days-to-a-big-idea-en"
 # get_full_book_data(book_slug)
-SLEEP = 0.5
+SLEEP = 0
 
 PM = ProxyManager.from_base_url
+
+FileUtils.rm_f("info.log")
 
 slugs = File.read("slugs.txt").split("\n")
 slugs.each do |slug|
